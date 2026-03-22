c4a493bfa9780aa396ef6626d762112c8aafe381
K S <skrishnasagar2004@gmail.com>
2026-03-22 20:19:34 +0530

    Added dependency checks for sherlock

--- oscan/modules/username/sherlock_search.py
+++ oscan/modules/username/sherlock_search.py
@@ -3,18 +3,22 @@ import time
 import shutil
 
 def search_sherlock(username):
-    # --- Check if sherlock exists ---#
-    if not shutil.which("sherlock"):
-        return "[!] Sherlock not found. Install: pipx install sherlock-project"
 
-    command=["sherlock", username, "--print-found", "--no-txt"]
+    # --- Check if sherlock exists --- #
+    if not shutil.which("sherlock"):
+        return "[!] Sherlock not found. Install: pipx install sherlock-project", 0
+
+    command = ["sherlock", username, "--print-found", "--no-txt"]
+
     start_time = time.time()
 
     result = subprocess.run(
         command,
-	capture_output=True,
-	text=True
-        )
+        capture_output=True,
+        text=True
+    )
+
     end_time = time.time()
     scan_time = round(end_time - start_time, 2)
+
     return result.stdout, scan_time
